1a83c89d25c530b49cc8b8f163ce2d0adec469b1
Anurag019-hub <anurag160507@gmail.com>
2026-01-06 23:30:25 +0530

    add create user services

---
+++ Backend/Services/User.services.js
@@ -0,0 +1,19 @@
+import UserModel from "../models/User.model"
+
+export const CreateUserService = async (data) =>{
+    const {email,password} = data;
+    if(!email||!password){
+        throw new Error("Email and Password are required");
+    }
+    if(password.length()<6){
+        throw new Error("Password must be 6 charater long")
+    }
+    const existingUser =await UserModel.findOne({email});
+    if(existingUser){
+        throw new Error("Email already in use");
+    }
+    const HashPass = await UserModel.hashPassword(password);
+    const user =await UserModel.create({email,password:HashPass});
+    const token = user.generateJWT();
+    return {user,token};
+}
